5bfeae280b21f70e712d5988b10a7e9d05b51bf0
ArlonTuazon <nikka1378@yahoo.com>
2020-10-21 23:28:58 -0700

    generate functions

--- assets/js/passgen.js
+++ assets/js/passgen.js
@@ -1,3 +1,19 @@
-function GenPassword() {
-    window.alert ("Generate Password")
+window.alert ("Generate password now!");
+
+
+//Function for Generator
+function getLowerval () {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
+
+function getUpperval () {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getNumberval () {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function getSymbolval () {
+    var symbols = '!@#$%^&*(){}=<>/';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
