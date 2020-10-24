4a3dde800204e64a4be45e12ee5142b3a4f4be76
ArlonTuazon <nikka1378@yahoo.com>
2020-10-23 23:25:13 -0700

    Add options for length

--- assets/js/passgen.js
+++ assets/js/passgen.js
@@ -1,7 +1,56 @@
-window.alert ("Generate password now!");
+//window.alert ("Generate password now!");
+
+
+var pwLength;
+var getLowerval;
+var getUpperval;
+var getNumberval;
+var getSymbolval;    
+
+
+    
+var get = document.querySelector("#generate");
+get.addEventListener ("click", function () {
+    pw = generatePassword();
+    document.getElementById("result")=pw;
+});
+// Generate password function
+function generatePassword() {
+    pwLength = prompt("Enter Length of Password Length. Should be between 8 and 128 only.");
+     if (!pwLength) {
+         alert ("Please enter a number");
+      }
+      else if (pwLength < 8 || pwLength > 128) {
+         pwLength = prompt("You must choose between 8 and 128");     
+      }
+      else {
+          getLowerval = confirm ("Do you want with a Lowercase Letter?");
+          getUpperval = confirm ("Do you want with an Uppercase Leter");
+          getNumberval = confirm ("Dow want with Numbers?");
+          getSymbolval = confirm ("Do you want with Symbols?");
+      };
+
+      if (!getLowerval && !getUpperval && !getNumberval && !getSymbolval) {
+        choices = alert("You must choose a criteria!");
+
+    }
+    var password = [];
+    for (var i = 0; i < enter; i++) {
+        var pickChoices = choices[Math.floor(Math.random() * choices.length)];
+        password.push(pickChoices);
+    }
+    var pw = password.join("");
+    UserInput(pw);
+    return pw;
+}
+function UserInput(pw) {
+    document.getElementById("result").innerText = pw;
+
+}
 
 
 //Function for Generator
+  
 function getLowerval () {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
