9aeb31f298151f1600e60e819105b6862b31616d
ArlonTuazon <nikka1378@yahoo.com>
2020-10-25 00:34:26 -0700

    generate function

--- assets/js/passgen.js
+++ assets/js/passgen.js
@@ -1,58 +1,73 @@
-//window.alert ("Generate password now!");
-
+//Start declaring variables
+var generateBtn = document.querySelector("#generate");
 
 var pwLength;
-var getLowerval;
-var getUpperval;
-var getNumberval;
-var getSymbolval;    
+var Lowerval;
+var Upperval;
+var Numberval;
+var Symbolval;    
 
+var inputVal = true;
 
-    
-var get = document.querySelector("#generate");
-get.addEventListener ("click", function () {
-    pw = generatePassword();
-    document.getElementById("result")=pw;
-});
+//password input here
+
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+}
+function generatePassword() {
+    userPassword = "";
+    passwordCharacters = "";
+
 // Generate password function
 function generatePassword() {
     pwLength = prompt("Enter Length of Password Length. Should be between 8 and 128 only.");
      if (!pwLength) {
          alert ("Please enter a number");
       }
-      else if (pwLength < 8 || pwLength > 128) {
+       else if (pwLength < 8 || pwLength > 128) {
          pwLength = prompt("You must choose between 8 and 128");     
       }
       else {
-          getLowerval = confirm ("Do you want with a Lowercase Letter?");
-          getUpperval = confirm ("Do you want with an Uppercase Leter");
-          getNumberval = confirm ("Dow want with Numbers?");
-          getSymbolval = confirm ("Do you want with Symbols?");
+          Lowerval= confirm ("Do you want with a Lowercase Letter?");
+          Upperval = confirm ("Do you want with an Uppercase Letter?");
+          Numberval = confirm ("Do you want with Numbers?");
+          Symbolval = confirm ("Do you want with Symbols?");
       };
 
-      if (!getLowerval && !getUpperval && !getNumberval && !getSymbolval) {
-        choices = alert("You must choose a criteria!");
+      if (!Lowerval && !Upperval && !Numberval && !Symbolval) {
+        inputVal = alert("You must choose a criteria!");
 
     }
+      else if (inputVal == true) {
+        getLowerval ();
+        getUpperval();
+        getSymbolval;
+        getNumberval();
+        
+      }
     var password = [];
-    for (var i = 0; i < enter; i++) {
-        var pickChoices = choices[Math.floor(Math.random() * choices.length)];
+    for (var i = 0; i < pwLength; i++) {
+        var pickChoices = inputVal[Math.floor(Math.random() * inputVal.length)];
         password.push(pickChoices);
     }
-    var pw = password.join("");
-    UserInput(pw);
-    return pw;
+    //var pw = password.join("");
+    //UserInput(pw);
+    //return pw;
 }
 function UserInput(pw) {
-    document.getElementById("result").innerText = pw;
-
+     document.getElementById('result').innerHTML = pw;
 }
+
 
 
 //Function for Generator
   
 function getLowerval () {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    
 }
 
 function getUpperval () {
